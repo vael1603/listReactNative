c7b0524f6dc11833049956e7538a36692176c234
Elias valenzuela <elias.ariel.valenzuela@gmail.com>
2023-01-23 23:33:46 -0300

    lista

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  itemList = [{id: '01', value: '100', color: 'red'}, {id: '02', value: '200', color: 'green'}];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HOLA, CODER!</Text>
       <StatusBar style="auto" />
+
+      <FlatList style={styles.container}
+        data={itemList}
+          renderItem={data =>(
+            <TouchableOpacity>
+              <View backgroundColor={data.item.color}>
+                <Text>{data.item.value}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        keyExtractor={(item)=> item.id}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: 'center',
+    width: '100%'
   },
 });
